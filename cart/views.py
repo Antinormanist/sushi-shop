020c3fa6fb69a4f128b49e4ee3a67925c9ce48d4
Antinormanist <katya.goryacheva.2009@gmail.com>
2024-07-13 07:23:51 +1000

    User can put products into his cart

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 from .models import Cart
+from main.models import Sushi
 # Create your views here.
 def cart(request):
     context = {
@@ -9,3 +11,19 @@ def cart(request):
         'total_price': Cart.get_total_price(user=request.user),
     }
     return render(request, 'cart/cart.html', context)
+
+
+def order_make(request):
+    if request.method == 'POST':
+        name = request.POST.get('sushiName')
+        description = request.POST.get('sushiDescription')
+        amount = request.POST.get('amount')
+        if name and description and amount:
+            Cart.objects.create(
+                user=request.user,
+                amount=int(amount),
+                sushi=Sushi.objects.get(name=name, description=description)
+            )
+    
+        return JsonResponse({'status': 'success'})
+    return JsonResponse({'status': 'error'})
